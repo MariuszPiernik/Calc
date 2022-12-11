a3dfd4f38f56a74ec74f218f2a2c4b1ce468ec16
Mariusz Piernik <110234609+MariuszPiernik@users.noreply.github.com>
2022-12-11 16:19:06 +0100

    add class and gui

--- final.py
+++ final.py
@@ -1,3 +1,16 @@
-def main():
-    pass
+from decimal import Clamped
+import tkinter as tk
+
+class Calculator:
+    def _init_(self,win):
+        self.equationStrVar = tk.StringVar()
+        self.expressionStr = ""
+        self.calcKeyboard = [
+            ["7","8","9","+"],
+            ["4","5","6","-"],
+            ["1","2","3","*"],
+            ["0","Clear","=","/"]
+
+        ]
+
 
