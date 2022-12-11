e45cf01bfdd190e58693db8e321ec230631b92aa
Mariusz Piernik <110234609+MariuszPiernik@users.noreply.github.com>
2022-12-11 16:50:07 +0100

    end

--- final.py
+++ final.py
@@ -1,61 +1,64 @@
-from decimal import Clamped
+
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Calc")
-def main():
-    calc = Calculator(window)
+window.title("Calculator")
 
 class Calculator:
-    def _init_(self, win):
+    def __init__(self, win):
         self.equationStrVar = tk.StringVar()
         self.expressionStr = ""
         self.calcKeyboard = [
-            ["7", "8", "9", "+"],
+            ["7", "8", "9","+"],
             ["4", "5", "6", "-"],
-            ["1", "2", "3", "*"],
-            ["0", "Clear", "=", "/"],
+            ["1", "2" ,"3" ,"*"],
+            ["0", "Clear", "=", "/"]
+
         ]
         self.prepareGui(win)
 
-    def prepareGui(self, win):
+    def prepareGui(self,win):
         win.geometry("260x130")
         self.expressionField = tk.Entry(window, textvariable=self.equationStrVar)
-        self.expressionField.grid(columnspan=4, ipadx=60)
+        self.expressionField.grid(columnspan= 4, ipadx= 60)
+
         rowIndex = 0
         while rowIndex < len(self.calcKeyboard):
             calcRow = self.calcKeyboard[rowIndex]
-            columnIndex = 0
-        while columnIndex < len(calcRow):
-            buttonText = calcRow[columnIndex]
-            button = tk.Button(
-                win,
-                text=buttonText,
-                height=1,
-                width=8,
-                fg="black",
-                command=lambda v=buttonText: self.buttonPressed(v),
 
-            )
-            columnIndex += 1
-        rowIndex += 1
-    def buttonPressed(self,value):
+            columnIndex = 0
+            while columnIndex < len(calcRow):
+                
+                buttonText = calcRow[columnIndex]
+                button = tk.Button(win, text= buttonText, height= 1, width= 8, fg = "black", bg ="silver", 
+                command = lambda v=buttonText: self.buttonPressed(v))
+
+                button.grid(row = rowIndex+1, column= columnIndex)
+
+                columnIndex += 1
+            
+            rowIndex += 1
+        
+    def buttonPressed(self, value):
+            
         print("button pressed: ", value)
 
         if value == "Clear":
-            self.equationStr = ""
+            self.expressionStr = ""
             self.equationStrVar.set("")
             return
         if value == "=":
-            result =  str(eval(self.expressionStr))
+            result = str(eval(self.expressionStr))
             self.expressionStr = result
             self.equationStrVar.set(result)
             return
-        self.expressionStr += str(value)
+        self.expressionStr +=  str(value)
         self.equationStrVar.set(self.expressionStr)
+        
+        
+
+
+clac = Calculator(window)
 
 
 window.mainloop()
-
-if __name__ == "__main__":
-    main()
